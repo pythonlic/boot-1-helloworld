6f51fdaccf7f9537e6d78ead16037ab85e55bd2a
lcl <2328638898@qq.com>
2022-10-04 14:52:43 +0800

    second commit

--- src/main/java/com/atguigu/boot/bean/Car.java
+++ src/main/java/com/atguigu/boot/bean/Car.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 @EqualsAndHashCode
 @ConfigurationProperties(prefix = "mycar")
 public class Car {
+    // v1
     private  String brand;
     private  Integer price;
 }
